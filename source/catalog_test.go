02a0ad31bb36afb9d6d7b6b090dde24e00396c9f
chcthink <chenprg@gmail.com>
2024-12-29 21:08:31 +0800

    test: rm cookie

--- source/catalog_test.go
+++ source/catalog_test.go
@@ -28,7 +28,7 @@ func TestGetCatalog(t *testing.T) {
 		ef.BookConf = &models.BookConf{
 			Catalog: models.UrlWithCookie{
 				Url:    "https://www.qidian.com/book/1035420986/",
-				Cookie: "e1=%7B%22l6%22%3A%221%22%2C%22l7%22%3A%22%22%2C%22l1%22%3A%22%22%2C%22l3%22%3A%22%22%2C%22pid%22%3A%22qd_P_xiangqing%22%2C%22eid%22%3A%22%22%7D; e2=%7B%22l6%22%3A%221%22%2C%22l7%22%3A%22%22%2C%22l1%22%3A%22%22%2C%22l3%22%3A%22%22%2C%22pid%22%3A%22qd_P_xiangqing%22%2C%22eid%22%3A%22%22%7D; newstatisticUUID=1728217849_1979792855; _csrfToken=OKiYHYB6eLNnXewMkNxbA67cFsgwhIkuCjp6qmEo; fu=1949850521; supportWebp=true; traffic_search_engine=; se_ref=; e1=%7B%22l6%22%3A%22%22%2C%22l7%22%3A%22%22%2C%22l1%22%3A3%2C%22l3%22%3A%22%22%2C%22pid%22%3A%22qd_p_qidian%22%2C%22eid%22%3A%22qd_A17%22%7D; e2=%7B%22l6%22%3A%22%22%2C%22l7%22%3A%22%22%2C%22l1%22%3A3%2C%22l3%22%3A%22%22%2C%22pid%22%3A%22qd_p_qidian%22%2C%22eid%22%3A%22qd_A16%22%7D; supportwebp=true; traffic_utm_referer=; x-waf-captcha-referer=; w_tsfp=ltvuV0MF2utBvS0Q6q/tk0quET0gfDo4h0wpEaR0f5thQLErU5mG1IJ9v8vwNXzd58xnvd7DsZoyJTLYCJI3dwMXR8WYJYlH3V6WwYd33olBCUExEJKKD1RKd+527DQXdXhCNxS00jA8eIUd379yilkMsyN1zap3TO14fstJ019E6KDQmI5uDW3HlFWQRzaLbjcMcuqPr6g18L5a5TrctlquKFx9UbhF1EHBhysXXXpw4EO7Ju0LNxz7d8enSqA=",
+				Cookie: "",
 			},
 		}
 		err := GetCatalogFromUrl(&ef)
